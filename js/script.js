8bbdfdabf1f911ce296ee5de212069a45288dfe1
Elcho911 <boss.armsport@gmail.com>
2024-02-25 04:41:30 +0600

    added select

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-const getLeads = async () => {
+const getLeadsAndContacts = async (entityType, limit) => {
 	const response = await fetch(
-		`${cors_proxy}https://${subdomain}.amocrm.ru/api/v4/leads`,
+		`${cors_proxy}https://${subdomain}.amocrm.ru/api/v4/${entityType}?limit=${limit}`,
 		{
 			method: 'GET',
 			headers: {
@@ -9,26 +9,58 @@ const getLeads = async () => {
 		}
 	);
 	const responseData = await response.json();
-	return responseData._embedded.leads;
+	return responseData._embedded[entityType];
 };
 
-const renderData = async () => {
-	const usersData = await getLeads();
-	console.log(usersData);
-
-	const renderData = usersData.map(
-		(item) =>
-			`
-				<tr>
-					<td>${item.name}</td>
-					<td>${item.price}</td>
-					<td>${item.name}</td>
-					<td>${new Date(item.created_at * 1000).toLocaleString()}</td>
-					<td>${new Date(item.updated_at * 1000).toLocaleString()}</td>
-				</tr>
-			`
+const mergeLeadsAndContacts = (leadsData, contactsData) => {
+	const mergedData = [];
+	const contactsMap = new Map(
+		contactsData.map((contact) => [contact.created_at, contact])
 	);
-	const getHtml = document.getElementById('tableBody');
-	getHtml.innerHTML = renderData.join('');
+
+	for (const lead of leadsData) {
+		const contact = contactsMap.get(lead.created_at);
+		if (contact) {
+			mergedData.push({
+				...lead,
+				leadName: lead.name,
+				contactName: contact.name
+			});
+		} else {
+			mergedData.push({ ...lead, leadName: lead.name, contactName: '' });
+		}
+	}
+
+	return mergedData;
 };
-renderData();
+
+const renderData = async (limit) => {
+	const leadsData = await getLeadsAndContacts('leads', limit);
+	const contactsData = await getLeadsAndContacts('contacts', limit);
+
+	const mergedData = mergeLeadsAndContacts(leadsData, contactsData);
+
+	const renderData = mergedData.map((lead) => {
+		return `
+			<tr>
+				<td>${lead.leadName}</td>
+				<td>${lead.price}</td>
+				<td>${lead.contactName}</td>
+				<td>${new Date(lead.created_at * 1000).toLocaleString()}</td>
+				<td>${new Date(lead.updated_at * 1000).toLocaleString()}</td>
+			</tr>
+		`;
+	});
+
+	const tableBody = document.getElementById('tableBody');
+	tableBody.innerHTML = renderData.join('');
+};
+
+document
+	.getElementById('paginationSelect')
+	.addEventListener('change', (event) => {
+		const limit = parseInt(event.target.value);
+		renderData(limit);
+	});
+
+renderData(5);
